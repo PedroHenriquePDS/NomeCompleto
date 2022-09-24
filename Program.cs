9ee56f25f52a6a6b4bef077122cf91099b661be4
Pedro Henrique <Pedrohenrique053010@gmail.com>
2022-09-23 22:23:30 -0300

    adiciona a solução ao exercicio Nome Completo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Digite seu nome e sobrenome: ");
+
+string Nome = Console.ReadLine()!;
+
+string PrimeiroNome = Nome.Substring(0, Nome.IndexOf(" "));
+string Segundonome = Nome.Substring(Nome.IndexOf(" ") + 1);
+
+System.Console.WriteLine();
+    System.Console.WriteLine("Nome completo: " + PrimeiroNome + " " + Segundonome);
+System.Console.WriteLine();
+    System.Console.WriteLine("Nome de catálogo: " + Segundonome + ", " + PrimeiroNome);
+System.Console.WriteLine();
+    System.Console.WriteLine("Pressione uma tecla para fechar...");
+Console.ReadKey();
+
+
